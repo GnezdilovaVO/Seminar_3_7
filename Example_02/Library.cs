8180a86b9b520f892b4a4e2dd2b75be3655c4ec3
Anton Vorontsov <antvork@gmail.com>
2023-03-16 15:12:19 +0700

    add task 2

---
+++ Example_02/Library.cs
@@ -0,0 +1,21 @@
+public static class Library
+{
+    int[,] GetProdutMatrix(int[,] array1,int[,] array2)
+    {
+        int[,] result =new int [,];
+        int sum = 0;
+        for(int i = 0; i<array1.GetLength(0); i++)
+        {
+            for(int j = 0; j<array1.GetLength(1); j++)
+            {
+                for(int k = 0; k<array1.GetLength(0); k++)
+                {
+                    
+                }
+            }
+        }
+    }
+
+
+
+}
