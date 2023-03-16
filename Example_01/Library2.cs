e305863f2835a8cee364d464f9c83ac78fa10bfd
Valentina <valyapomortseva@gmail.com>
2023-03-16 14:30:41 +0700

    Создала метод печати массива

---
+++ Example_01/Library2.cs
@@ -0,0 +1,14 @@
+public static class Library2
+{
+    public static void PrintArray(int[,] array)
+    {
+        for (int i=0; i<array.GetLength(0); i++)
+        {
+            for (int j =0; j <array.GetLength(1);j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
